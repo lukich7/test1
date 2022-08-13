dcc6ef6c6129d4a1d5e2049192077386b9f38c0c
lukyanenko01 <v.lukyanenko0101@gmail.com>
2022-08-13 10:02:01 +0300

    next commit

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Lukich on 13.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("fff")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
